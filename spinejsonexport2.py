95ae3e830644b79c942dc70bcd76412f2471a473
绘星痕 <2805137028@qq.com>
2025-07-18 04:46:02 +0800

    优化音频的中文名匹配

--- spinejsonexport2.py
+++ spinejsonexport2.py
@@ -1,8 +1,7 @@
 import subprocess
-from os import path, getcwd, remove, walk
+from os import path, remove, walk
 import json
 from concurrent.futures import ThreadPoolExecutor
-from loguru import logger
 from config_manager import ConfigManager
 from pathlib import Path
 
@@ -11,26 +10,6 @@ SPINE_EXE = cfg.get("spine_path")
 ffm_path = str(cfg.get("ffm_path"))
 with open("res/template.export.json", 'r', encoding='utf-8') as f:
     ejtdata = json.load(f)
-
-
-def convert_mov_to_mp4(mov_path):
-    """使用 ffmpeg 将 .mov 转换为 .mp4 格式"""
-    output_file = path.splitext(mov_path)[0] + ".mp4"
-    cmd = [
-        ffm_path, "-i", str(mov_path),
-        "-c:v", "libx264", "-crf", "23", "-c:a", "aac", "-strict", "experimental", str(output_file)
-    ]
-    logger.info(f"🚀 转换 MOV 到 MP4: {path.basename(mov_path)}")
-    try:
-        result = subprocess.run(cmd, capture_output=True, text=True, encoding="utf-8", errors="replace")
-        if result.returncode == 0:
-            logger.success(f"✅ 转换成功: {output_file}")
-        else:
-            logger.error(f"❌ 转换失败 (退出码: {result.returncode})")
-            if result.stderr:
-                logger.error(f"错误信息:\n{result.stderr.strip()}")
-    except Exception as e:
-        logger.error(f"❌ 转换过程中出错: {e}")
 
 
 def exportSpineJson(input_json_path, out_path=None):
@@ -89,35 +68,12 @@ def exportSpineJson(input_json_path, out_path=None):
         return
     remove(Export_JSON)
     remove(OUTPUT_SPINE)
-    mov_file = ejdata["output"]
-    if path.isfile(mov_file):
-        convert_mov_to_mp4(mov_file)
-    else:
-        mov_list = []
-
-        # 遍历文件夹
-        for root, dirs, files in walk(mov_file):
-            for file in files:
-                # 检查文件扩展名是否为.mov（不区分大小写）
-                if file.lower().endswith('.mov'):
-                    # 获取文件的完整路径并添加到列表
-                    full_path = path.join(root, file)
-                    mov_list.append(full_path)
-
-        with ThreadPoolExecutor(max_workers=4) as executor:
-            futures = [
-                executor.submit(convert_mov_to_mp4, i)
-                for i in mov_list
-            ]
-            # 可选：等待所有任务完成（with 语句会自动等待）
-            for future in futures:
-                future.result()  # 检查是否有异常
 
 
 def sjemain():
     max_workers = cfg.get("max_workers")
 
-    with ThreadPoolExecutor(max_workers=max_workers) as executor: # type: ignore
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:  # type: ignore
         # 提交所有任务到线程池，并传入索引 i
         futures = [
             executor.submit(exportSpineJson, json_file, None)
